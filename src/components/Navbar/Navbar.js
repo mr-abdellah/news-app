c0e629b9c311275105ccaa1e549bf64bce876c28
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-09-24 14:42:24 +0100

    Add navbar for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,32 @@
 import "./Navbar.css";
 import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const close = () => {
+    setOpen(false);
+  }
   return (
     <nav id="Navbar">
       <div id="Logo">
-        <Link to='/home'>N</Link>
+        <Link to="/home">N</Link>
       </div>
-      <ul>
-        <NavLink to="/home">HOME</NavLink>
-        <NavLink to="/sports">SPORTS</NavLink>
-        <NavLink to="/science">SCIENCE</NavLink>
-        <NavLink to="/technology">TECHNOLOGY</NavLink>
+      <ul className={open ? "nav-links-show" : "nav-links"}>
+        <NavLink onClick={close} to="/home">HOME</NavLink>
+        <NavLink onClick={close} to="/sports">SPORTS</NavLink>
+        <NavLink onClick={close} to="/science">SCIENCE</NavLink>
+        <NavLink onClick={close} to="/technology">TECHNOLOGY</NavLink>
       </ul>
+      <div id="hamburger" onClick={() => setOpen(!open)}>
+        {open ? (
+          <FontAwesomeIcon icon={faXmark} />
+        ) : (
+          <FontAwesomeIcon icon={faBars} />
+        )}
+      </div>
     </nav>
   );
 };
