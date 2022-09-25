64739cec077d75b3d44ad1f8b75ee498ed2646d9
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-09-25 08:20:10 +0100

    Add responsive sports  page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './pages/homepage/Homepage';
+import SportsPage from './pages/sports/SportsPage';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/home" element={<Homepage />} />
-          <Route path="/sports" element={<div>sports</div>} />
+          <Route path="/sports" element={<SportsPage />} />
           <Route path="/science" element={<div>science</div>} />
           <Route path="/technology" element={<div>technology</div>} />
         </Routes>
---
+++ src/pages/sports/SportsPage.js
@@ -0,0 +1,30 @@
+import "../Page.css";
+import { useSportsNewsQuery } from ".././../redux/newsSlice";
+
+const SportsPage = () => {
+  const { isError, isLoading, isSuccess, data } = useSportsNewsQuery();
+  const sportsNews = data?.articles;
+  console.log(sportsNews);
+  return (
+    <div id="SportsPage">
+      <h1>Sports News</h1>
+      <div className="posts">
+        {isSuccess &&
+          sportsNews.map((article, index) => (
+            <div className="homepage_post post" key={index}>
+              <img src={article?.urlToImage} alt={article?.title} />
+              <p>{article?.publishedAt.slice(0, 10)}</p>
+              <h4>{article?.title}</h4>
+              <p>
+                <a href={article.url} target="_blank">
+                  {article.description}
+                </a>
+              </p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SportsPage;
